8526cd7edcd62cd9e2ff502805970449ba3ee8cc
bobomurod <bobomurod.boboev@yandex.ru>
2020-11-14 04:18:44 +0500

    conroller check done

--- src/routers/TodosRouter.ts
+++ src/routers/TodosRouter.ts
@@ -4,6 +4,7 @@ import TodosController from '../controllers/TodosController'
 const router = Router()
 const todosController = new TodosController();
 
-router.get('/todos', todosController.get);
+
+router.get('/', todosController.get);
 
 export default router;
