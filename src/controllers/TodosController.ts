f4d30d31038ddf4ab4fcd33188cd748d66feec3b
bobomurod <bobomurod.boboev@yandex.ru>
2020-11-15 23:44:41 +0500

    new route and method

--- src/controllers/TodosController.ts
+++ src/controllers/TodosController.ts
@@ -6,13 +6,28 @@ class TodosController {
         try {
             const client = await pool.connect();
 
-            const sql = "SELECT * FROM todos";
+            const sql = "SELECT * FROM test_table";
             const { rows } = await client.query(sql);
             const todos = rows;
 
             client.release();
 
             res.send(todos);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    }
+
+    public async post(req: Request, res: Response) {
+        try {
+            const client = await pool.connect();
+
+            const sql = `INSERT INTO test_schema."test_table" ("name", "isActive") VALUES ('new task', FALSE)`;
+            const result  = await client.query(sql);
+
+            client.release();
+
+            res.send(result);
         } catch (error) {
             res.status(400).send(error);
         }
